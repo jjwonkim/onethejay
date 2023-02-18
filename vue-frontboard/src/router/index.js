52678e14e107c818e79967154398ab2488ee3014
Jaewon Kim <jjwon.kim@gmail.com>
2023-02-18 14:22:29 +0900

    4 - 게시판 목록 화면 구성 https://onethejay.tistory.com/66

--- vue-frontboard/src/router/index.js
+++ vue-frontboard/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import PageHome from '@/views/PageHome.vue'
+import BoardList from '@/views/board/BoardList.vue'
 
 const routes = [
   {
@@ -9,12 +10,18 @@ const routes = [
   },
   {
     path: '/about',
-    name: 'About',
+    name: 'PageAbout',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/PageAbout.vue')
+  },
+  {
+    path: '/board/list',
+    name: 'BoardList',
+    component: BoardList
   }
+
 ]
 
 const router = createRouter({
